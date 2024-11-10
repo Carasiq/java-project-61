dc43b04672beb5ee5438670436bf8fc1f155549e
dusar <dusar.00@bk.ru>
2024-11-10 18:40:11 +0300

    add "package hexelet.code" in App.class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,4 +1,6 @@
-
-public static void main(String[] args) {
-    System.out.println("Welcome to the Brain Games!");
+package hexlet.code;
+class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Brain Games!");
+    }
 }
