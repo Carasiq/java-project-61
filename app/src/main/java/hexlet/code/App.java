44e4845dd8a01942d5a02284230e7f1d1c950e4c
dusar <dusar.00@bk.ru>
2024-11-22 16:31:06 +0300

    files has been moved

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,12 +2,18 @@ package hexlet.code;
 import java.util.Scanner;
 
 class App {
+    public static int gameNumber;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet\n" + "2 - Even\n" + "0 - Exit");
-        System.out.print("Your choice: ");
-        int gameNumber = scanner.nextInt();
+        System.out.println("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                2 - Even
+                3 - Calc
+                0 - Exit""");
+        System.out.print("\nYour choice: ");
+        gameNumber = scanner.nextInt();
 
         if (gameNumber == 1) {
             sayName();
@@ -15,15 +21,21 @@ class App {
 
         if (gameNumber == 2) {
             sayName();
-            Even.parityCheck();
+            Engine.parityCheck();
+        }
+        if (gameNumber == 3) {
+            sayName();
+            Engine.parityCheck();
         }
     }
-    public static String userName;
+
     public static void sayName() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("\nWelcome to the Brain Games!");
-        System.out.println("Say I have your name?");
-        userName = scanner.next();
-        System.out.println("Hello, " + userName + "!");
+        System.out.println("""
+                
+                Welcome to the Brain Games!
+                Say I have your name?""");
+        Engine.userName = scanner.next();
+        System.out.println("Hello, " + Engine.userName + "!");
     }
 }
